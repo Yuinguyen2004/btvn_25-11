927682811d93bf3f54c9881989ca431f9f827d45
Admin <Admin@DESKTOP-8I6P477>
2024-11-25 06:19:12 +0700

    Add project files.

---
+++ btvn/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace btvn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lv_account = new System.Windows.Forms.ListView();
+            this.cl_name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cl_pass = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.cl_phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_them = new System.Windows.Forms.Button();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.btn_sua = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_name = new System.Windows.Forms.TextBox();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lv_account
+            // 
+            this.lv_account.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.cl_name,
+            this.cl_pass,
+            this.cl_phone});
+            this.lv_account.FullRowSelect = true;
+            this.lv_account.GridLines = true;
+            this.lv_account.HideSelection = false;
+            this.lv_account.Location = new System.Drawing.Point(179, 140);
+            this.lv_account.Name = "lv_account";
+            this.lv_account.Size = new System.Drawing.Size(202, 217);
+            this.lv_account.TabIndex = 0;
+            this.lv_account.UseCompatibleStateImageBehavior = false;
+            this.lv_account.View = System.Windows.Forms.View.Details;
+            this.lv_account.SelectedIndexChanged += new System.EventHandler(this.lv_account_SelectedIndexChanged);
+            // 
+            // cl_name
+            // 
+            this.cl_name.Text = "Name";
+            // 
+            // cl_pass
+            // 
+            this.cl_pass.Text = "password";
+            // 
+            // cl_phone
+            // 
+            this.cl_phone.Text = "phone";
+            // 
+            // btn_them
+            // 
+            this.btn_them.Location = new System.Drawing.Point(441, 140);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(75, 51);
+            this.btn_them.TabIndex = 1;
+            this.btn_them.Text = "thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            this.btn_them.Click += new System.EventHandler(this.btn_them_Click);
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.Location = new System.Drawing.Point(441, 222);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(75, 51);
+            this.btn_xoa.TabIndex = 2;
+            this.btn_xoa.Text = "xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            this.btn_xoa.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btn_sua
+            // 
+            this.btn_sua.Location = new System.Drawing.Point(441, 306);
+            this.btn_sua.Name = "btn_sua";
+            this.btn_sua.Size = new System.Drawing.Size(75, 51);
+            this.btn_sua.TabIndex = 3;
+            this.btn_sua.Text = "sửa";
+            this.btn_sua.UseVisualStyleBackColor = true;
+            this.btn_sua.Click += new System.EventHandler(this.btn_sua_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(565, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(565, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(565, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Phone:";
+            // 
+            // txt_name
+            // 
+            this.txt_name.Location = new System.Drawing.Point(651, 154);
+            this.txt_name.Name = "txt_name";
+            this.txt_name.Size = new System.Drawing.Size(100, 22);
+            this.txt_name.TabIndex = 7;
+            // 
+            // txt_password
+            // 
+            this.txt_password.Location = new System.Drawing.Point(651, 195);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(100, 22);
+            this.txt_password.TabIndex = 8;
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(651, 239);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(100, 22);
+            this.txt_phone.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.txt_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_sua);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.lv_account);
+            this.Name = "Form1";
+            this.Text = "Thêm, Xóa, Sửa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lv_account;
+        private System.Windows.Forms.ColumnHeader cl_name;
+        private System.Windows.Forms.ColumnHeader cl_pass;
+        private System.Windows.Forms.ColumnHeader cl_phone;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.Button btn_xoa;
+        private System.Windows.Forms.Button btn_sua;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_name;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.TextBox txt_phone;
+    }
+}
+
---
+++ btvn/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace btvn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (lv_account.SelectedItems.Count > 0)
+            {
+                lv_account.Items.Remove(lv_account.SelectedItems[0]);
+            }
+        }
+
+        private void btn_them_Click(object sender, EventArgs e)
+        {
+            ListViewItem lv = new ListViewItem(txt_name.Text);
+           
+            lv.SubItems.Add(txt_password.Text);
+            lv.SubItems.Add(txt_phone.Text);
+
+            lv_account.Items.Add(lv);
+        }
+
+        private void lv_account_SelectedIndexChanged(object sender, EventArgs e)
+        {
+              
+        }
+
+        private void btn_sua_Click(object sender, EventArgs e)
+        {
+            if(lv_account.SelectedItems.Count > 0)
+            {
+                ListViewItem selecteditem = lv_account.SelectedItems[0];
+
+                selecteditem.SubItems[0].Text= txt_name.Text;
+                selecteditem.SubItems[1].Text = txt_password.Text;
+                selecteditem.SubItems[2].Text = txt_phone.Text;
+
+            }    
+        }
+    }
+}
